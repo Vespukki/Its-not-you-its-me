c9fffa2b05715dbc48ea1e45e16146b197648003
Vespukki <106420039+Vespukki@users.noreply.github.com>
2024-09-14 23:10:12 -0500

    red screen

--- Assets/Scripts/AngerSetter.cs
+++ Assets/Scripts/AngerSetter.cs
@@ -2,12 +2,24 @@ using PixelCrushers.DialogueSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AngerSetter : MonoBehaviour
 {
+    Image image;
+    private void Awake()
+    {
+     image = GetComponent<Image>();
+
+    }
+
     void Update()
     {
         DialogueLua.SetVariable("AngerWaitTime", DialogueLua.GetVariable("Anger").AsFloat * DialogueLua.GetVariable("Timeout").AsFloat);
 
+        var startColor = image.color;
+
+        image.color = new Vector4(image.color.r, image.color.g, image.color.b, DialogueLua.GetVariable("Anger").AsFloat);
+        
     }
 }
